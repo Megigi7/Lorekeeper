9c05a6e333b9dc6a1dae7327aaada59214361418
María <mariaam1005@gmail.com>
2026-07-14 09:16:53 +0200

    borrar vistas temporales

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -14,7 +14,7 @@
             <a href="{{ url('/') }}"> ▸ Home</a>
             <a href="{{ url('characters') }}"> ▸ Characters</a>
             <a href="{{ url('relationships') }}"> ▸ Relationships</a>
-            <a href="{{ route('character_species.index') }}">App Configuration</a>
+            <a href="{{ route('app_configuration.index') }}">App Configuration</a>
         </div>
     </div>
 
